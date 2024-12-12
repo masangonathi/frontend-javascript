0408655074a686d7aacae9196d907dd877aed64b
masangonathi <nkosinathimasango@hotmail.com>
2024-12-12 10:18:35 +0200

    initial commit

---
+++ task_0/js/main.ts
@@ -0,0 +1,43 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+  }
+  
+  const student1: Student = {
+    firstName: "Sbani",
+    lastName: "Masango",
+    age: 20,
+    location: "Durban",
+  };
+  
+  const student2: Student = {
+    firstName: "Sphe",
+    lastName: "Masango",
+    age: 22,
+    location: "Jozi",
+  };
+  
+  const studentsList: Student[] = [student1, student2];
+  
+  const table = document.createElement("table");
+  const tbody = document.createElement("tbody");
+  
+  studentsList.forEach((student) => {
+    const row = document.createElement("tr");
+  
+    const nameCell = document.createElement("td");
+    nameCell.textContent = student.firstName;
+  
+    const locationCell = document.createElement("td");
+    locationCell.textContent = student.location;
+  
+    row.appendChild(nameCell);
+    row.appendChild(locationCell);
+    tbody.appendChild(row);
+  });
+  
+  table.appendChild(tbody);
+  document.body.appendChild(table);
+  
